2dad7a905677525bc3eca2b1073309f7e833f50f
Bob Cohen <bobcohen1969@yahoo.com>
2016-10-03 09:55:32 -0500

    menus set up, create robot complete, start at option 2, list robots

---
+++ java-track/exercises/RobotMenu.java
@@ -0,0 +1,99 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class RobotMenu {
+
+	/*
+	 * has a
+	 * 1. list of Robots created
+	 * 2. Scanner object used for accepting user input
+	 * 
+	 * Behaviors
+	 * when menu starts, prints out list of available options
+	 * menu can accept an input from the user indicating which option they would like to perform, account for invalid options
+	 * once valid option accepted, menu to ask user which robot to manipulate
+	 * 
+	 */
+
+	private ArrayList<Robots> robots;
+	private Scanner s;
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//bring up menu and asks for option
+		int x = 0;
+		RobotMenu rm = new RobotMenu();
+		do {
+			x = rm.startMenu();
+			rm.processInput(x);
+		} while (x != 6);
+	}
+
+
+	public RobotMenu()
+	//Constructor statements:  make sure instance variables have values.
+	{
+		s = new Scanner(System.in);
+		robots = new ArrayList<Robots>();
+	}
+
+	public int startMenu()
+	//display menu
+	{
+		System.out.println("Welcome to the robots menu!");
+		System.out.println("1. Create a Robot");
+		System.out.println("2. Display the list of avialable robots");
+		System.out.println("3. Move a robot");
+		System.out.println("4. Rotate a robot");
+		System.out.println("5. Compute the distance between two robots");
+		System.out.println("6. Exit");
+		System.out.println("Please select an option:  ");
+		//takes in the selection
+		int selection = s.nextInt();
+		//validate input
+		while(selection < 0 || selection > 6)
+		
+		{
+			System.out.println("Invalid selection, please try again:  ");
+			selection = s.nextInt();
+		}
+
+		return selection;
+	}
+
+
+	public void processInput(int selection){
+		//processing of the input from the menu
+
+		//declarations
+
+			String name;
+			int posX;
+			int posY;
+			int speed;
+			int orientation;
+		
+			{
+				if(selection == 1){
+				System.out.println("Enter a name for the robot: ");
+				name = s.next();
+				System.out.println("Enter an x position for the robot:  ");
+				posX = s.nextInt();
+				System.out.println("Enter a y position for the robot:  ");
+				posY = s.nextInt();
+				System.out.println("Enter a speed for the robot:  ");
+				speed = s.nextInt();
+				System.out.println("Enter an orientation for the robot(0 = North, 90 = East, 180 = South, 270 = West):  ");
+				orientation = s.nextInt();
+
+				new Robots(name, posX, posY, speed, orientation);
+			}
+				
+				//Continue with Display list of robots, options 2
+
+		}
+	}
+}
+
